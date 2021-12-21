dd5e66f96c038ddcbacfd02312ae3a1d6a459c70
YuanZeXu <jeffrey0420@gmail.com>
2021-12-21 11:29:25 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,25 @@
+let 頻道號碼 = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (頻道號碼))
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.South)
+    radio.sendString("LUCKY")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (頻道號碼))
+    basic.clearScreen()
+})
+radio.onReceivedString(function (receivedString) {
+    while (true) {
+        basic.showString(receivedString)
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    頻道號碼 = randint(1, 30)
+    radio.setGroup(頻道號碼)
+    basic.showArrow(ArrowNames.East)
+    basic.pause(200)
+    basic.showString("" + (頻道號碼))
+    basic.clearScreen()
+})
